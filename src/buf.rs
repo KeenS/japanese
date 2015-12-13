4aec33e39e73e53c03cadc88361d91f71bf68318
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-12-13 23:57:14 +0900

    tiny changes

--- src/buf.rs
+++ src/buf.rs
@@ -2,36 +2,38 @@ extern crate encoding;
 use self::encoding::{EncodingRef, DecoderTrap};
 use std::io::Read;
 use std::io;
-use std::vec::Vec;
 
 
-pub struct EncodingReader<R> {
+pub struct DecodingReader<R> {
     inner: R,
-    decoding: EncodingRef,
+    encoding: EncodingRef,
 }
 
-impl <R: Read> EncodingReader<R> {
+impl <R: Read> DecodingReader<R> {
     pub fn new(coding: EncodingRef, read: R)-> Self {
-        EncodingReader {
+        DecodingReader {
             inner: read,
-            decoding: coding
+            encoding: coding
         }
     }
 }
 
-impl <R: Read> Read for EncodingReader<R> {
+impl <R: Read> Read for DecodingReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize>{
-        use std::iter::repeat;
-        // TODO: choose appropriate size
-        let mut vec = repeat(0).take(buf.len()/2).collect::<Vec<u8>>();
-        let mut b = &mut vec[..];
-        let _ = try!(self.inner.read(&mut b));
+        // TODO set appropriate buffer size
+        let len = buf.len() / 2;
+        let n = try!(self.inner.read(&mut buf[..len/2]));
         // TODO don't ignore input code
-        let str = self.decoding.decode(b, DecoderTrap::Ignore).unwrap();
-        let bytes = str.into_bytes();
-        for (d, s) in buf.iter_mut().zip(bytes.iter()) {
-            *d = *s;
+        let str = self.encoding.decode(&mut buf[..n], DecoderTrap::Ignore).unwrap();
+        let mut i = 0;
+        for (d, s) in buf.iter_mut().zip(str.bytes()) {
+            *d = s;
+            i+=1;
+            if i == 0 {
+                break
+            };
         }
-        Ok(b.len())
+        assert!(i <= buf.len());
+        Ok(i)
     }
 }
